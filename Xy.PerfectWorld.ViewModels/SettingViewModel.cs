dc958fa38145e729f8db8c2173224f2032ac5e92
Xiaoy312 <Xiaoyao312@gmail.com>
2015-07-18 20:17:11 -0400

    splitted SettingViewModel into various partial classes

--- Xy.PerfectWorld.ViewModels/SettingViewModel.cs
+++ Xy.PerfectWorld.ViewModels/SettingViewModel.cs
@@ -11,41 +11,16 @@ using Xy.PerfectWorld.Models;
 
 namespace Xy.PerfectWorld.ViewModels
 {
-    public class SettingViewModel : ReactiveObject
+    public partial class SettingViewModel : ReactiveObject
     {
-        public ReactiveList<GameModel> RunningGames { get; }
-
-        GameModel selectedGame;
-        public GameModel SelectedGame
-        {
-            get { return selectedGame; }
-            set { this.RaiseAndSetIfChanged(ref selectedGame, value); }
-        }
-
-        public ReactiveCommand<object> AttachToGame { get; }
-
         public SettingViewModel()
         {
-            RunningGames = new ReactiveList<GameModel>();
-
-            Observable.Interval(TimeSpan.FromSeconds(1), RxApp.MainThreadScheduler)
-                .Subscribe(_ => RefreshGames());
-
-            RunningGames.CountChanged
-                .Where(count => count != 0 && SelectedGame == null)
-                .Subscribe(_ => SelectedGame = RunningGames.FirstOrDefault());
+            InitializeClient();
+            InitializeAutoCombat();
         }
 
-        public void RefreshGames()
-        {
-            var exited = RunningGames.Where(x => x.Process.HasExited).ToList();
-            RunningGames.RemoveAll(exited);
-
-            Process.GetProcessesByName("elementclient")
-                .Where(p => !RunningGames.Any(g => p.Id == g.Process.Id))
-                .Select(x => new GameModel(x))
-                .ForEach(RunningGames.Add);
-        }
+        partial void InitializeClient();
+        partial void InitializeAutoCombat();
     }
 
 

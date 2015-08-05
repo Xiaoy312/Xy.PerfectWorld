a38417d390310e1bff46befa08ad4cf300e87898
Xiaoy312 <Xiaoyao312@gmail.com>
2015-08-05 08:32:22 -0400

    added call for casting

--- Xy.PerfectWorld.Models/Call.cs
+++ Xy.PerfectWorld.Models/Call.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xy.DataAnalysis;
 using Xy.DataAnalysis.Legacy;
 
 namespace Xy.PerfectWorld.Models
@@ -30,7 +31,6 @@ namespace Xy.PerfectWorld.Models
             }
             asm.Run(item.LootBase.Core);
         }
-
         public static void Target(this Npc npc)
         {
             var asm = new ASM();
@@ -51,7 +51,6 @@ namespace Xy.PerfectWorld.Models
             }
             asm.Run(npc.NpcBase.Core);
         }
-
         public static void Attack(this Character character)
         {
             var asm = new ASM();
@@ -65,7 +64,44 @@ namespace Xy.PerfectWorld.Models
                 asm.Ret();
             }
             asm.Run(character.CharacterBase.Core);
+        }
+        public static void Cast(Core core, int skillID)
+        {
+            //00492AFA    8B0D D46F9200     mov ecx,dword ptr ds:[926FD4]              ; elementc.00927630
+            //00492B00    8B40 08           mov eax,dword ptr ds:[eax+8]
+            //00492B03    6A FF             push -1
+            //00492B05    6A 00             push 0
+            //00492B07    8B51 1C           mov edx,dword ptr ds:[ecx+1C]
+            //00492B0A    6A 00             push 0
+            //00492B0C    50                push eax
+            //00492B0D    8B4A 20           mov ecx,dword ptr ds:[edx+20]
+            //00492B10    E8 BB2EFCFF       call elementc.004559D0
 
+            var asm = new ASM();
+            {
+                asm.Pushad();
+
+                asm.Push6A(-1);
+                asm.Push6A(0);
+                asm.Push6A(0);
+                asm.Mov_EDX(skillID);
+                asm.Push_EDX();
+
+                asm.Mov_ECX_DWORD_Ptr(Game.GameBaseAddress);
+                asm.Mov_ECX_DWORD_Ptr_ECX_Add(0x1C);
+                asm.Mov_ECX_DWORD_Ptr_ECX_Add(0x20);
+
+                asm.Mov_EAX(0x004559D0);
+                asm.Call_EAX();
+
+                asm.Popad();
+                asm.Ret();
+            };
+            asm.Run(core);
+        }
+        public static void Cast(this Skill skill)
+        {
+            Cast(skill.SkillBase.Core, skill.SkillID);
         }
     }
 }
